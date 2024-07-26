209974ad0484ef379486bac19588bd2736dde530
harley.li <fleaislhl@gmail.com>
2024-07-26 16:04:29 +0800

    add device check

--- spiltvoice.py
+++ spiltvoice.py
@@ -1,11 +1,18 @@
 import os.path
+import torch
 
 import demucs.api as demucsApi
-
+def get_available_device():
+    if torch.cuda.is_available():
+        return 'cuda'
+    elif hasattr(torch.backends, 'mps') and torch.backends.mps.is_available():
+        return 'mps'
+    else:
+        return 'cpu'
 def separate_audio(audio_path, output_dir,model,stem):
     print(model)
     print(stem)
-    separator = demucsApi.Separator(model=model, device='cpu', callback=cc)
+    separator = demucsApi.Separator(model=model, device=get_available_device(), callback=cc)
     origin, separated = separator.separate_audio_file(audio_path)
     # 遍历 separated 字典并保存每个音频源
     for stem, tensor in separated.items():
